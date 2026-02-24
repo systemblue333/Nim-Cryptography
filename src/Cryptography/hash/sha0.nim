4dbdf53aca131d4249833f091fac85624a5b5ea9
systemblue333 <systemblue.dev@proton.me>
2026-02-24 08:45:29 +0000

    Refactor SHA-0 implementation for clarity and efficiency

--- src/Cryptography/hash/sha0.nim
+++ src/Cryptography/hash/sha0.nim
@@ -4,14 +4,16 @@ import ../../../../Utility/src/Utility/codeutils/indexutils
 import ../../../../Utility/src/Utility/codeutils/bits
 import ../../../../Utility/src/Utility/codeutils/errorutils
 import ../../../../Utility/src/Utility/dataformat/dataformat
+import bitops
+import std/[monotimes, times]
 
 # common sha0 operating process template
-template process(chunk: lent openArray[uint8], state: var array[5, uint32]): void =
+template sha0Transform(state: var array[5, uint32], input: lent openArray[uint8]): void =
   # declare extended chunk
   var w: array[80, uint32]
 
   # copy chunk to extended chunk(w)
-  discard decodeBE(chunk, w.toOpenArray(0, 15), 16)
+  decodeBE(input, w.toOpenArray(0, 15), 16)
 
   # extend chunk to extended chunk
   for i in static(16..<80):
@@ -42,10 +44,10 @@ template process(chunk: lent openArray[uint8], state: var array[5, uint32]): voi
       f = b xor c xor d
       k = 0xca62c1d6'u32
 
-    let temp = leftRotate(a, 5) + f + e + k + w[i]
+    let temp = rotateLeftBits(a, 5) + f + e + k + w[i]
     e = d
     d = c
-    c = leftRotate(b, 30)
+    c = rotateLeftBits(b, 30)
     b = a
     a = temp
 
@@ -86,7 +88,7 @@ template sha0OneC(input: lent openArray[uint8]): array[20, uint8] =
 
   # break chunk 512bits
   for chunkStart in countup(0, buffer.len - 64, 64):
-    process(buffer[chunkStart..<chunkStart + 64], state)
+    sha0Transform(state, buffer[chunkStart..<chunkStart + 64])
 
   # encode state to output
   encodeBE(state, output)
@@ -102,34 +104,36 @@ when Bits == 64:
   # SHA-0 context for 64 bits
   type
     SHA0Ctx* = object
-      intermediateHash*: array[5, uint32]
+      state*: array[5, uint32]
       bitLength*: uint64
-      messageBlockIndex*: int
-      messageBlock*: array[64, uint8]
+      index*: int
+      buffer*: array[64, uint8]
       computed*: bool
 else:
   # SHA-0 context for 32 or lower bits
   type
     SHA0Ctx* = object
-      intermediateHash*: array[5, uint32]
+      state*: array[5, uint32]
       bitLength*: array[2, uint32]
-      messageBlockIndex*: int
-      messageBlock*: array[64, uint8]
+      index*: int
+      buffer*: array[64, uint8]
       computed*: bool
 
 # sha0 init core
 template sha0InitC(ctx: var SHA0Ctx): void =
   when Bits == 64:
     ctx.bitLength = 0
-  else:
+  elif Bits == 32:
     for i in static(0 ..< 2):
-      bitLength[i] = 0
-  ctx.messageBlockIndex = 0
-  ctx.intermediateHash[0] = 0x67452301'u32
-  ctx.intermediateHash[1] = 0xefcdab89'u32
-  ctx.intermediateHash[2] = 0x98badcfe'u32
-  ctx.intermediateHash[3] = 0x10325476'u32
-  ctx.intermediateHash[4] = 0xc3d2e1f0'u32
+      ctx.bitLength[i] = 0
+  ctx.index = 0
+  for i in static(0 ..< 64):
+    ctx.buffer[i] = 0x00'u8
+  ctx.state[0] = 0x67452301'u32
+  ctx.state[1] = 0xefcdab89'u32
+  ctx.state[2] = 0x98badcfe'u32
+  ctx.state[3] = 0x10325476'u32
+  ctx.state[4] = 0xc3d2e1f0'u32
   ctx.computed = false
 
 # sha0 input core
@@ -150,31 +154,31 @@ template sha0InputC(ctx: var SHA0Ctx, input: openArray[uint8]): void =
     when Bits == 64:
       ctx.bitLength += uint64(inputLen shl 3)
     # 32/lower bits version
-    else:
+    elif Bits == 32:
       let oldLow = ctx.bitLength[0]
       ctx.bitLength[0] += uint32((inputLen shl 3) and 0xFFFFFFFF'u32)
       if ctx.bitLength[0] < oldLow:
         ctx.bitLength[1] += 1
       ctx.bitLength[1] += uint32((inputLen shl 3) shr 32)
 
-    if ctx.messageBlockIndex > 0:
-      let space: int = 64 - ctx.messageBlockIndex
+    if ctx.index > 0:
+      let space: int = 64 - ctx.index
       let fill: int = min(inputLen, space)
 
-      copyMem(addr ctx.messageBlock[ctx.messageBlockIndex], addr input[0], fill)
+      copyMem(addr ctx.buffer[ctx.index], addr input[0], fill)
 
-      if ctx.messageBlockIndex == 64:
-        process(ctx.messageBlock, ctx.intermediateHash)
-        ctx.messageBlockIndex = 0
+      if ctx.index == 64:
+        sha0Transform(ctx.state, ctx.buffer)
+        ctx.index = 0
 
       while index <= inputLen - 64:
-        process(input.toOpenArray(index, index + 63), ctx.intermediateHash)
+        sha0Transform(ctx.state, input.toOpenArray(index, index + 63))
         index += 64
 
-    let rem = inputLen - index
-    if rem > 0:
-      copyMem(addr ctx.messageBlock[0], unsafeAddr input[index], rem)
-      ctx.messageBlockIndex = rem
+    let remain = inputLen - index
+    if remain > 0:
+      copyMem(addr ctx.buffer[0], unsafeAddr input[index], remain)
+      ctx.index = remain
 
 # sha0 finalize core
 template sha0FinalC(ctx: var SHA0Ctx): array[20, uint8] =
@@ -183,45 +187,45 @@ template sha0FinalC(ctx: var SHA0Ctx): array[20, uint8] =
 
   # check computed state and encode
   if ctx.computed:
-    encodeBE(ctx.intermediateHash, output)
+    encodeBE(ctx.state, output)
     check = false
 
   if check:
     # do padding(add 0x80 and zero)
-    ctx.messageBlock[ctx.messageBlockIndex] = 0x80
-    ctx.messageBlockIndex += 1
+    ctx.buffer[ctx.index] = 0x80
+    ctx.index += 1
 
     # if messageBlockIndex is bigger then 56
-    if ctx.messageBlockIndex > 56:
+    if ctx.index > 56:
       # add 0 while messageBlockIndex is smaller then 64
-      while ctx.messageBlockIndex < 64:
-        ctx.messageBlock[ctx.messageBlockIndex] = 0
-        ctx.messageBlockIndex += 1
+      while ctx.index < 64:
+        ctx.buffer[ctx.index] = 0
+        ctx.index += 1
       # call process
-      process(ctx.messageBlock, ctx.intermediateHash)
-      ctx.messageBlockIndex = 0
+      sha0Transform(ctx.state, ctx.buffer)
+      ctx.index = 0
 
     # add 0 whil messageBlockIndex is smaller then 56
-    while ctx.messageBlockIndex < 56:
-      ctx.messageBlock[ctx.messageBlockIndex] = 0
-      ctx.messageBlockIndex += 1
+    while ctx.index < 56:
+      ctx.buffer[ctx.index] = 0
+      ctx.index += 1
 
     # add bit length to end by big endian
     when Bits == 64:
       for i in static(0 ..< 8):
-        ctx.messageBlock[56 + i] = uint8(ctx.bitLength shr ((7 - i) * 8) and 0xFF)
+        ctx.buffer[56 + i] = uint8(ctx.bitLength shr ((7 - i) * 8) and 0xFF)
     else:
       for i in static(0 ..< 4):
-        ctx.messageBlock[56 + i] = uint8(ctx.bitLength[0] shr ((7 - i) * 8) and 0xFF)
-      for i in static(4 ..< 8):
-        ctx.messageBlock[56 + i] = uint8(ctx.bitLength[1] shr ((7 - i) * 8) and 0xFF)
+        ctx.buffer[56 + i] = uint8(ctx.bitLength[0] shr ((3 - i) * 8) and 0xFF)
+      for i in static(0 ..< 4):
+        ctx.buffer[60 + i] = uint8(ctx.bitLength[1] shr ((3 - i) * 8) and 0xFF)
 
     # process and encoding part
-    process(ctx.messageBlock, ctx.intermediateHash)
+    sha0Transform(ctx.state, ctx.buffer)
     ctx.computed = true
 
     # encode intermediateHash to output
-    encodeBE(ctx.intermediateHash, output)
+    encodeBE(ctx.state, output)
 
   output
 
@@ -254,3 +258,16 @@ when defined(test):
   echo "SHA0One : ", binToHex(sha0One(s))
   echo "SHA0 Standard : 5A5588F0407C6AE9A988758E76965F841B299229"
   echo "Input : Hello, World!"
+
+  template benchmark(name: string, code: untyped) =
+    let start = getMonoTime()
+    code
+    let elapsed = getMonoTime() - start
+    echo name, " took: ", elapsed.inMicroseconds, " μs (", elapsed.inNanoseconds, " ns)"
+  var a: array[20, uint8]
+  var ctx2: SHA0Ctx
+  benchmark("SHA-0 Benchamark"):
+    for i in 1 .. 1_000_000:
+      sha0Init(ctx2)
+      sha0Input(ctx2, a)
+      a = sha0Final(ctx2)
